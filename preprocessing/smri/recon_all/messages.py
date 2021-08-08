6b1693bbd06507826cb94a2c23b4d2207dfd40fe
Zvi Baratz <z.baratz@gmail.com>
2021-08-08 15:14:53 +0300

    Added docstrings and removed messages module.

--- preprocessing/smri/recon_all/messages.py
+++
@@ -1,9 +0,0 @@
-"""
-ReconAll automation module messages.
-"""
-from preprocessing.smri.recon_all.utils import bcolors
-
-#: ReconAll not registered in the database.
-NO_RECON_ALL_VERSIONS = "No ReconAll versions registered in the database!"
-
-# flake8: noqa: E501
